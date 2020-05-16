3e3f006a8fa5481d402369db2d27baa73d4a5a41
Anıl Öztürk <iletisim@baykatre.com>
2020-05-16 16:49:58 +0300

    Update Service.js
    URL update

--- src/service/Service.js
+++ src/service/Service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // let BASE_URL = 'https://baykatre.com/earthquake-dataset.json'
-let BASE_URL_NEW = 'https://baykatre.com/csvjson.json'
+let BASE_URL_NEW = 'https://anilozturk.co/csvjson.json'
 class Service {
     
     fetchDataset = () => {
